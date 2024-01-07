172bd99d0016921562ae79044b07c803e4f80e70
tal rodgold <talrodgold@gmail.com>
2024-01-07 19:13:01 +0200

    task 2

---
+++ bot.py
@@ -0,0 +1,10 @@
+class bot:
+    _language="english"
+def __init__(self,sender,maxlength=50):
+    self.sender=sender
+    self.maxlength=maxlength
+def send(self,message):
+    if len(message)<=self.maxlength:
+        print(f"sending the message {message} on behalf of{self.sender} using {bot._language}")
+    else:
+        print("it's too long, can't send")
---
+++ main.py
@@ -0,0 +1,8 @@
+from bot import bot
+def main():
+    b=bot("lev",40)
+    b.send("Israel will win")
+    b.send("Israel is winning "*2)
+    b.send("""Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    Fusce ac facilisis mauris, in lacinia ante. Sed sit amet nisl ante.
+    Pellentesque ut eleifend sem. Nulla eleifend neque non """)
